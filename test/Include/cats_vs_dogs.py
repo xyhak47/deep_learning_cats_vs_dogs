b1261aceec29bbb7d88f3d1c436fa14c921abb36
yhxiao <yhxiao@usens.com>
2019-07-19 17:48:43 +0800

    update

--- test/Include/cats_vs_dogs.py
+++ test/Include/cats_vs_dogs.py
@@ -159,54 +159,138 @@ for data_batch, labels_batch in train_generator:
     break
 
 
-# there are 2000 images in train_dir, batch_size is 20, so steps_per_epoch = 2000 / 20 = 100
-# there are 1000 images in validation_dir, batch_size is 20, so validation_steps = 1000 / 20 = 50
-history = model.fit_generator(
-    train_generator,
-    steps_per_epoch=100,
-    epochs=30,
-    validation_data=validation_generator,
-    validation_steps=50
-)
 
-model.save(join_path(base_dir, 'cats_and_dogs_small_1.h5'))
+model_save_path_1 = join_path(base_dir, 'cats_and_dogs_small_1.h5')
+
+if not os.path.exists(model_save_path_1):
+    # there are 2000 images in train_dir, batch_size is 20, so steps_per_epoch = 2000 / 20 = 100
+    # there are 1000 images in validation_dir, batch_size is 20, so validation_steps = 1000 / 20 = 50
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=100,
+        epochs=30,
+        validation_data=validation_generator,
+        validation_steps=50)
+    model.save(model_save_path_1)
+    draw_data()
 
 
 #draw to show
 import matplotlib.pyplot as plt
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
+def draw_data():
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
 
-epochs = range(1, len(acc) + 1)
+    epochs = range(1, len(acc) + 1)
 
-plt.plot(epochs, acc, 'bo', label='Training acc')
-plt.plot(epochs, val_acc, 'b', label='Validation acc')
-plt.legend()
+    plt.plot(epochs, acc, 'bo', label='Training acc')
+    plt.plot(epochs, val_acc, 'b', label='Validation acc')
+    plt.legend()
 
-plt.figure()
+    plt.figure()
 
-plt.plot(epochs, loss, 'bo', label='Training loss')
-plt.plot(epochs, val_loss, 'b', label='Validation loss')
-plt.legend()
+    plt.plot(epochs, loss, 'bo', label='Training loss')
+    plt.plot(epochs, val_loss, 'b', label='Validation loss')
+    plt.legend()
 
-plt.show()
+    plt.show()
 
 
 
 #---------------------------------------------------------------------------------
 #we will do it again with data augmentation:
-
+datagen = ImageDataGenerator(
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest'
+)
 
 # here is an example of data augmentation:
 from keras.preprocessing import image
 
-fnames = [join_path(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
 
-img_path = fnames[3] #pick up one image
-img = image.load_img(img_path, target_size=(150,150))
-x = image.img_to_array(img) # -> (150,150,3) numpy array
-x = x.reshape((1,) + x.shape) # -> (1,150,150,3)
+def draw_data2():
+    fnames = [join_path(train_cats_dir, fname) for fname in os.listdir(train_cats_dir)]
 
+    img_path = fnames[3] #pick up one image
+    img = image.load_img(img_path, target_size=(150,150))
+    x = image.img_to_array(img) # -> (150,150,3) numpy array
+    x = x.reshape((1,) + x.shape) # -> (1,150,150,3)
+
+    i = 0
+    for batch in datagen.flow(x, batch_size=1):
+        plt.figure(i)
+        imgplot = plt.imshow(image.array_to_img(batch[0]))
+        i += 1
+        if i%4 == 0:
+            break
+
+    plt.show()
+
+
+draw_data2()
+
+
+# new model with adding dropout and using augmentation data
+model = models.Sequential();
+model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(150,150,3)))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(64, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Conv2D(128, (3,3), activation='relu'))
+model.add(layers.MaxPooling2D((2,2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))   # <--------------------change here
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy',
+              optimizer=optimizers.RMSprop(lr=1e-4),
+              metrics=['acc'])
+
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True
+)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    train_dir,
+    target_size=(150,150),
+    batch_size=32,
+    class_mode='binary'
+)
+
+validation_generator = validation_datagen.flow_from_directory(
+    validation_dir,
+    target_size=(150,150),
+    batch_size=32,
+    class_mode='binary'
+)
+
+model_save_path_2 = join_path(base_dir, 'cats_and_dogs_small_2.h5')
+
+if not os.path.exists(model_save_path_2):
+    history = model.fit_generator(
+        train_generator,
+        steps_per_epoch=100,
+        epochs=100,
+        validation_data=validation_generator,
+        validation_steps=50)
+    model.save(model_save_path_2)
+    draw_data()
